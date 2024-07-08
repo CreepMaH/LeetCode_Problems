822159d99b72333f1da0e53b8c17c81730e76dcc
Сергей Винчестер <creepmah@gmail.com>
2024-07-08 20:09:18 +0300

    Решение №3

---
+++ LeetCode_Problems/N3_LongestSubstring.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode_Problems
+{
+    public class N3_LongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxLength = 0;
+
+            int left = 0;
+            int right = 0;
+
+            HashSet<char> hashSet = [];
+            List<char> list = [];
+
+            while (right < s.Length)
+            {
+                list.Add(s[right]);
+                if (hashSet.Add(s[right]))
+                {
+                    if (hashSet.Count > maxLength)
+                    {
+                        maxLength++;
+                    }
+                }
+                else
+                {
+                    int duplicateIndex = list.FindIndex(c => c == s[right]);
+
+                    while (duplicateIndex >= 0)
+                    {
+                        hashSet.Remove(list[0]);
+                        list.RemoveAt(0);
+                        duplicateIndex--;
+                    }
+                    
+                    hashSet.Add(s[right]);
+                    left++;
+                }
+
+                right++;
+            }
+
+            return maxLength;
+        }
+    }
+}
