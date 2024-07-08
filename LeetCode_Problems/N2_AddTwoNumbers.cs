b9892f7931a7da2f55d0755ef04bc513c08c722e
Сергей Винчестер <creepmah@gmail.com>
2024-07-08 18:53:49 +0300

    Пройдены тесты №2.

--- LeetCode_Problems/N2_AddTwoNumbers.cs
+++ LeetCode_Problems/N2_AddTwoNumbers.cs
@@ -15,41 +15,34 @@
 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            List<ListNode> result = new()
-            {
-                new ListNode()
-            };
+            ListNode resultNode = new();
+            ListNode currentNode = resultNode;
 
             ListNode? addNode1 = l1;
             ListNode? addNode2 = l2;
             int transferDigit = 0;
 
-            while (addNode1 != null || addNode2 != null)
+            bool addNextNode = true;
+
+            while (addNextNode)
             {
                 int sum = (addNode1?.val ?? 0) + (addNode2?.val ?? 0) + transferDigit;
 
-                var currentNode = result.Last();
-
                 currentNode.val = sum % 10;
+                transferDigit = sum / 10;
 
                 addNode1 = addNode1?.next;
                 addNode2 = addNode2?.next;
 
-                if (addNode1 != null || addNode2 != null)
+                addNextNode = addNode1 != null || addNode2 != null || transferDigit != 0;
+                if (addNextNode)
                 {
                     currentNode.next = new ListNode();
-                    result.Add(currentNode.next);
-
-                    transferDigit = sum / 10;
-                }
-                else if (sum / 10 > 0)
-                {
-                    result.Last().next = new ListNode(transferDigit);
-                    result.Add(result.Last().next);
+                    currentNode = currentNode.next;
                 }
             }
 
-            return result[0];
+            return resultNode;
         }
     }
 }
