6e023493e9f751e205025a72958f4adb8e70d38f
Сергей Винчестер <creepmah@gmail.com>
2024-07-11 09:15:59 +0300

    Тесты на №4

---
+++ LeetCode_Problems/N4_MedianOfTwoSortedArrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Problems
+{
+    public class N4_MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+
+        }
+    }
+}
---
+++ Tests/N4_MedianOfTwoSortedArrays_Test.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode_Problems;
+
+namespace Tests
+{
+    internal class N4_MedianOfTwoSortedArrays_Test
+    {
+        private N4_MedianOfTwoSortedArrays _n4_MedianOfTwoSortedArrays;
+
+        [SetUp]
+        public void Setup()
+        {
+            _n4_MedianOfTwoSortedArrays = new N4_MedianOfTwoSortedArrays();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int[] nums1 = [1, 3];
+            int[] nums2 = [2];
+
+            double actual = _n4_MedianOfTwoSortedArrays.FindMedianSortedArrays(nums1, nums2);
+            double expected = 2;
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] nums1 = [1, 2];
+            int[] nums2 = [3, 4];
+
+            double actual = _n4_MedianOfTwoSortedArrays.FindMedianSortedArrays(nums1, nums2);
+            double expected = 2.5;
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
