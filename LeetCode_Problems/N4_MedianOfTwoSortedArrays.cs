3627b455a6e472334e0b7828cc9473f60c1a5446
Сергей Винчестер <creepmah@gmail.com>
2024-08-29 09:18:47 +0300

    Ход решения

--- LeetCode_Problems/N4_MedianOfTwoSortedArrays.cs
+++ LeetCode_Problems/N4_MedianOfTwoSortedArrays.cs
@@ -10,7 +10,28 @@ namespace LeetCode_Problems
     {
         public double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
+            
+        }
 
+        private int[] GetMedianValues(int[] nums)
+        {
+            if (nums.Length % 2 == 0)
+            {
+                int medianIndex2 = nums.Length / 2;
+                return [nums[medianIndex2 - 1], nums[medianIndex2]];
+            }
+            else
+            {
+                return [nums[(nums.Length - 1) / 2]];
+            }
+        }
+
+        private int? TryGetMedian(int[] medianValues1, int[] medianValues2)
+        {
+            if (medianValues1.Length == 2 && medianValues2.Length == 2)
+            {
+
+            }
         }
     }
 }
