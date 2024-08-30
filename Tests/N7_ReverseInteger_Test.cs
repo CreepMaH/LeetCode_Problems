ced26e74ec89452c411813ec5c4459a0bb2a4b35
CreepMaH <creepmah@gmail.com>
2024-08-30 09:57:00 +0300

    Решение: Runtime 62%, Memory 37%.

---
+++ Tests/N7_ReverseInteger_Test.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode_Problems;
+
+namespace Tests
+{
+    public class N7_ReverseInteger_Test
+    {
+        readonly N7_ReverseInteger _n7_ReverseInteger;
+
+        public N7_ReverseInteger_Test()
+        {
+            _n7_ReverseInteger = new N7_ReverseInteger();
+        }
+
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        public void Test1(int x, int expected)
+        {
+            int actual = _n7_ReverseInteger.Reverse(x);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
