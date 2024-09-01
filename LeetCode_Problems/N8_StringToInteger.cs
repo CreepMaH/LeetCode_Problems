49b95504d91d83b0db85a55d7049147c244e32ed
Сергей Винчестер <creepmah@gmail.com>
2024-09-01 18:35:35 +0300

    Решение №8. Не нравится: надо оптимизировать и отрефакторить.

---
+++ LeetCode_Problems/N8_StringToInteger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Problems
+{
+    public class N8_StringToInteger
+    {
+        public int MyAtoi(string s)
+        {
+            s = s.Trim();
+            bool isNegative = s.StartsWith('-');
+            if (isNegative || s.StartsWith('+'))
+            {
+                s = s[1..];
+            }
+
+            int maxAbsValue = int.MaxValue / 10;
+
+            int result = 0;
+            int digitPow = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    int digitToAdd = (int)char.GetNumericValue(s[i]);
+
+                    if (digitToAdd == 0 && result == 0)
+                    {
+                        continue;
+                    }
+
+                    if (result > maxAbsValue || result < -maxAbsValue)
+                    {
+                        result = isNegative ? int.MinValue : int.MaxValue;
+                        break;
+                    }
+                    else if (Math.Abs(result) == maxAbsValue)
+                    {
+                        if (isNegative && digitToAdd >= 8)
+                        {
+                            result = int.MinValue;
+                            break;
+                        }
+                        else if (!isNegative && digitToAdd >= 7)
+                        {
+                            result = int.MaxValue;
+                            break;
+                        }
+                        else
+                        {
+                            result *= 10;
+                            result += (isNegative ? (-1) : 1) * digitToAdd;
+                            digitPow++;
+                        }
+                    }
+                    else
+                    {
+                        result *= 10;
+                        result += (isNegative ? (-1) : 1) * digitToAdd;
+                        digitPow++;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/N8_StringToInteger_Test.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode_Problems;
+
+namespace Tests
+{
+    public class N8_StringToInteger_Test
+    {
+        private readonly N8_StringToInteger _n8_StringToInteger;
+
+        public N8_StringToInteger_Test()
+        {
+            _n8_StringToInteger = new();
+        }
+
+        [TestCase("42", 42)]
+        [TestCase("-042", -42)]
+        [TestCase("1337c0d3", 1337)]
+        [TestCase("0-1", 0)]
+        [TestCase("words and 987", 0)]
+        [TestCase("-91283472332", -2147483648)]
+        [TestCase("2147483646", 2147483646)]
+        [TestCase("-2147483649", -2147483648)]
+        public void Test1(string s, int expected)
+        {
+            var actual = _n8_StringToInteger.MyAtoi(s);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
