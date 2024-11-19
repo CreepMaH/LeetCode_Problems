a5af5cded24ce5fe5d01927b3ba0e41574ba4ece
Сергей Винчестер <creepmah@gmail.com>
2024-11-19 21:48:24 +0300

    58% runtime, 46% memory

---
+++ LeetCode_Problems/N10-19/N14_LongestCommonPrefix.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode_Problems
+{
+    public class N14_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            int minLength = strs.Min(str => str.Length);
+
+            StringBuilder builder = new ();
+
+            for (int i = 0; i < minLength; i++)
+            {
+                char currentChar = strs[0][i];
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != currentChar)
+                    {
+                        return builder.ToString();
+                    }
+                }
+
+                builder.Append(currentChar);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
