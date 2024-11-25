def2ab1646873a9396fa317145886b026c294ebd
Сергей Винчестер <creepmah@gmail.com>
2024-11-25 09:58:04 +0300

    Рабочее решение полным перебором с оптимизацией, но неоптимальное: 5% runtime, 5% memory.

---
+++ LeetCode_Problems/N10-19/N16_3SumClosest.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace LeetCode_Problems
+{
+    public class N16_3SumClosest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            nums = [.. nums.Order()];
+
+            int left, right, current = 0;
+            int minDiff = int.MaxValue;
+            int closestSum = 0;
+
+            while (current < nums.Length - 2)
+            {
+                left = current + 1;
+                right = nums.Length - 1;
+
+                int lastDiff = int.MaxValue;
+
+                for (int i = left; i < right; i++)
+                {
+                    for (int j = right; j > left; j--)
+                    {
+                        int sum = nums[current] + nums[i] + nums[j];
+                        int currentDiff = Math.Abs(target - sum);
+
+                        if (currentDiff < minDiff)
+                        {
+                            minDiff = currentDiff;
+                            closestSum = sum;
+                            lastDiff = currentDiff;
+                        }
+                        else if (currentDiff > lastDiff)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                current++;
+            }
+
+            return closestSum;
+        }
+    }
+}
