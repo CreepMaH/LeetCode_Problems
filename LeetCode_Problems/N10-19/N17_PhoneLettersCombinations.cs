c516562f9ef42b217710ab65c3a3acdc14ba1a4d
Сергей Винчестер <creepmah@gmail.com>
2024-12-16 11:18:41 +0300

    Accepted: 10% runtime, 15% memory.

---
+++ LeetCode_Problems/N10-19/N17_PhoneLettersCombinations.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace LeetCode_Problems
+{
+    public class N17_PhoneLettersCombinations
+    {
+        private readonly Dictionary<int, string[]> _keyMap = new()
+        {
+            { 2, ["a", "b", "c"] },
+            { 3, ["d", "e", "f"] },
+            { 4, ["g", "h", "i"] },
+            { 5, ["j", "k", "l"] },
+            { 6, ["m", "n", "o"] },
+            { 7, ["p", "q", "r", "s"] },
+            { 8, ["t", "u", "v"] },
+            { 9, ["w", "x", "y", "z"] }
+        };
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            List<string> combinations = [];
+
+            foreach (char dig in digits)
+            {
+                List<string> newCombinations = [];
+                var letters = _keyMap[Convert.ToInt32(dig.ToString())];
+
+                foreach (string lett in letters)
+                {
+                    if (combinations.Count > 0)
+                    {
+                        foreach (string comb in combinations)
+                        {
+                            newCombinations.Add($"{comb}{lett}");
+                        }
+                    }
+                    else
+                    {
+                        newCombinations.Add(lett);
+                    }
+                }
+
+                combinations.Clear();
+                combinations.AddRange(newCombinations);
+            }
+
+            combinations.Sort();
+            return [.. combinations];
+        }
+    }
+}
