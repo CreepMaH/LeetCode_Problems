8d677a2e8c8439cf38a7a7badd4b744e057cf15b
Сергей Винчестер <creepmah@gmail.com>
2024-12-16 12:36:35 +0300

    Backtracking: 100% runtime, 35% memory.

--- LeetCode_Problems/N10-19/N17_PhoneLettersCombinations.cs
+++ LeetCode_Problems/N10-19/N17_PhoneLettersCombinations.cs
@@ -4,48 +4,43 @@ namespace LeetCode_Problems
 {
     public class N17_PhoneLettersCombinations
     {
-        private readonly Dictionary<int, string[]> _keyMap = new()
+        private List<string> _combinations = [];
+        private readonly Dictionary<char, string[]> _keyMap = new()
         {
-            { 2, ["a", "b", "c"] },
-            { 3, ["d", "e", "f"] },
-            { 4, ["g", "h", "i"] },
-            { 5, ["j", "k", "l"] },
-            { 6, ["m", "n", "o"] },
-            { 7, ["p", "q", "r", "s"] },
-            { 8, ["t", "u", "v"] },
-            { 9, ["w", "x", "y", "z"] }
+            { '2', ["a", "b", "c"] },
+            { '3', ["d", "e", "f"] },
+            { '4', ["g", "h", "i"] },
+            { '5', ["j", "k", "l"] },
+            { '6', ["m", "n", "o"] },
+            { '7', ["p", "q", "r", "s"] },
+            { '8', ["t", "u", "v"] },
+            { '9', ["w", "x", "y", "z"] }
         };
 
         public IList<string> LetterCombinations(string digits)
         {
-            List<string> combinations = [];
-
-            foreach (char dig in digits)
+            _combinations = [];
+            if (!string.IsNullOrEmpty(digits))
             {
-                List<string> newCombinations = [];
-                var letters = _keyMap[Convert.ToInt32(dig.ToString())];
-
-                foreach (string lett in letters)
-                {
-                    if (combinations.Count > 0)
-                    {
-                        foreach (string comb in combinations)
-                        {
-                            newCombinations.Add($"{comb}{lett}");
-                        }
-                    }
-                    else
-                    {
-                        newCombinations.Add(lett);
-                    }
-                }
-
-                combinations.Clear();
-                combinations.AddRange(newCombinations);
+                BacktrackDigit("", digits);
             }
+            
+            return _combinations;
+        }
 
-            combinations.Sort();
-            return [.. combinations];
+        private void BacktrackDigit(string combination, string digitsRest)
+        {
+            if (string.IsNullOrEmpty(digitsRest))
+            {
+                _combinations.Add(combination);
+            }
+            else
+            {
+                foreach (string letter in _keyMap[digitsRest[0]])
+                {
+                    BacktrackDigit(combination + letter, digitsRest.Remove(0, 1));
+                }
+            }
         }
     }
 }
