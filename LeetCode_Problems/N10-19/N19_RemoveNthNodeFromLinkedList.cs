8e513484c50a14a5e76c441988c0aabd914ba031
Сергей Винчестер <creepmah@gmail.com>
2024-12-25 13:25:41 +0300

    100% runtime (not enough data), 81% memory

---
+++ LeetCode_Problems/N10-19/N19_RemoveNthNodeFromLinkedList.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode_Problems
+{
+    public class N19_RemoveNthNodeFromLinkedList
+    {
+        public ListNode? RemoveNthFromEnd(ListNode? head, int n)
+        {
+            Queue<ListNode?> queue = new();
+            queue.Enqueue(head);
+
+            ListNode? next = head?.next;
+            while (next != null)
+            {
+                queue.Enqueue(next);
+
+                if (queue.Count > n + 1)
+                {
+                    queue.Dequeue();
+                }
+
+                next = next.next;
+            }
+
+            ListNode? nodeToChange = queue.Dequeue();
+            if (queue.Count == n)
+            {
+                nodeToChange.next = queue.Dequeue()?.next;
+            }
+            else if (queue.Count > 0)
+            {
+                head = queue.Dequeue();
+            }
+            else
+            {
+                head = null;
+            }
+
+            return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
