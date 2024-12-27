6a4c1377675a84639531aaff35c9c515d1c64f01
Сергей Винчестер <creepmah@gmail.com>
2024-12-27 20:36:15 +0300

    28% runtime, 60% memory

---
+++ LeetCode_Problems/N20-29/N20_ValidParentheses.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode_Problems
+{
+    public class N20_ValidParentheses
+    {
+        Dictionary<char, char> _parenthesesPairs = new()
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+
+        public bool IsValid(string s)
+        {
+            Stack<char> parentheses = new();
+
+            foreach (char currentChar in s)
+            {
+                if (_parenthesesPairs.ContainsKey(currentChar))
+                {
+                    parentheses.Push(currentChar);
+                }
+                else
+                {
+                    bool popSuccess = parentheses.TryPop(out char popChar);
+                    if (!popSuccess || _parenthesesPairs[popChar] != currentChar)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parentheses.Count == 0;
+        }
+    }
+}
---
+++ Tests/N20-29/N20_ValidParentheses_Test.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode_Problems;
+
+namespace Tests
+{
+    public class N20_ValidParentheses_Test
+    {
+        private readonly N20_ValidParentheses _n20_ValidParentheses;
+
+        public N20_ValidParentheses_Test()
+        {
+            _n20_ValidParentheses = new N20_ValidParentheses();
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(string s, bool expected)
+        {
+            var actual = _n20_ValidParentheses.IsValid(s);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        public static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("()", true);
+                yield return new TestCaseData("()[]{}", true);
+                yield return new TestCaseData("(]", false);
+                yield return new TestCaseData("([])", true);
+            }
+        }
+    }
+}
