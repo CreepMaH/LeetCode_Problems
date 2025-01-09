1c79376b89fa8a8a4acfd6b2b71c6bdcf2022ae1
Сергей Винчестер <creepmah@gmail.com>
2025-01-09 11:26:46 +0300

    Recursion: 100% runtime, 28% memory.

---
+++ LeetCode_Problems/N20-29/N22_GenerateParenthesis.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode_Problems.Common;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
+namespace LeetCode_Problems
+{
+    internal class N22_GenerateParenthesis
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> combinations = [];
+            GenerateCombinations(combinations, "", 0, 0, n);
+            return combinations;
+        }
+
+        private void GenerateCombinations(List<string> combinations, string currentCombination, int leftCount, int rightCount, int n)
+        {
+            if (leftCount +  rightCount == n * 2)
+            {
+                combinations.Add(currentCombination);
+                return;
+            }
+
+            if (leftCount < n)
+            {
+                GenerateCombinations(combinations, currentCombination + "(", leftCount + 1, rightCount, n);
+            }
+
+            if (rightCount < leftCount)
+            {
+                GenerateCombinations(combinations, currentCombination + ")", leftCount, rightCount + 1, n);
+            }
+        }
+    }
+
+}
---
+++ Tests/N20-29/N22_GenerateParenthesis_Test.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode_Problems;
+
+namespace Tests
+{
+    internal class N22_GenerateParenthesis_Test
+    {
+        private readonly N22_GenerateParenthesis _n22_GenerateParenthesis;
+
+        public N22_GenerateParenthesis_Test()
+        {
+            _n22_GenerateParenthesis = new N22_GenerateParenthesis();
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test1(int n, IList<string> expected)
+        {
+            var actual = _n22_GenerateParenthesis.GenerateParenthesis(n);
+
+            Assert.That(expected, Is.EquivalentTo(actual));
+        }
+
+        internal static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    3,
+                    new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()"});
+
+                yield return new TestCaseData(
+                    1,
+                    new List<string> { "()" });
+            }
+        }
+    }
+}
