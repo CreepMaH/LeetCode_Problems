776322c97d931c975964da25e3707f879b0f2baa
Сергей Винчестер <creepmah@gmail.com>
2025-03-14 15:57:16 +0300

    100% runtime, 57% memory

---
+++ LeetCode_Problems/N20-29/N27_RemoveElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Tests")]
+namespace LeetCode_Problems.N20_29
+{
+    internal class N27_RemoveElement
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int diffsCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[diffsCount] = nums[i];
+                    diffsCount++;
+                }
+            }
+
+            return diffsCount;
+        }
+    }
+}
