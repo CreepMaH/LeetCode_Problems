77178fe8febfe8acb5d5b12d23261844daa981b7
Сергей Винчестер <creepmah@gmail.com>
2025-03-14 16:35:15 +0300

    100% runtime, 64% memory

---
+++ LeetCode_Problems/N20-29/N28_FindTheIndexOfFirstOccurence.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode_Problems.N20_29
+{
+    internal class N28_FindTheIndexOfFirstOccurence
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                int coincendenceLength = 0;
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (i + j >= haystack.Length)
+                    {
+                        return -1;
+                    }
+                    if (haystack[i + j] == needle[j])
+                    {
+                        coincendenceLength++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (coincendenceLength == needle.Length)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
