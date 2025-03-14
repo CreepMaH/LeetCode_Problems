070f2cb11df374116f40f8ee2a5d76fe444d557c
Сергей Винчестер <creepmah@gmail.com>
2025-03-14 15:44:07 +0300

    100 % runtime, 54% memory.

---
+++ LeetCode_Problems/N20-29/N26_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly:InternalsVisibleTo("Tests")]
+namespace LeetCode_Problems.N20_29
+{
+    internal class N26_RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int lastIndexOfUnique = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    lastIndexOfUnique++;
+                    nums[lastIndexOfUnique] = nums[i];
+                }
+            }
+
+            return lastIndexOfUnique + 1;
+        }
+    }
+}
